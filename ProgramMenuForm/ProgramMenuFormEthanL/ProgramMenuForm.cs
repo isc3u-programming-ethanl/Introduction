35cacc60295c186b694a5cd87211ee0c8bcbd1fc
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-09-09 12:23:23 -0400

    Added Program Menu

--- ProgramMenuForm/ProgramMenuFormEthanL/ProgramMenuForm.cs
+++ ProgramMenuForm/ProgramMenuFormEthanL/ProgramMenuForm.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * Created by: Ethan LeGault
+ * Created on: Sep. 6, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #4 - Hello World with Menu
+ * This program displays my favourite artist of all time.
+ * It also has an exit menu
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +33,7 @@ namespace ProgramMenuFormEthanL
 
         private void MniExit_Click(object sender, EventArgs e)
         {
+            //This when pressed makes the program shut down
             this.Close();
         }
     }
